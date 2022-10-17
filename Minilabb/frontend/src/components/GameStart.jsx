608959f963dc2953ca010449880b7938b9d9c0a6
L1nkzel <javalink@protonmail.com>
2022-10-17 13:55:17 +0200

    Created an input for creating users, and connected it to backend
    Co-authored-by: Semir Pecar <freemayne@users.noreply.github.com>
    Co-authored-by: alikeici <alikeici@users.noreply.github.com>

---
+++ Minilabb/frontend/src/components/GameStart.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import {Box, Button, Input, TextField} from "@mui/material"
+
+
+const GameStart = () => {
+
+    const [username, setUsername] = useState("") 
+
+    const handleChange = event => {
+      setUsername(event.target.value);
+      console.log(event.target.value);
+    }
+  
+    
+    const handleSave = async (event) => {
+        const data = {
+            username: username
+        }
+
+        await fetch("http://localhost:5000/user",
+        {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+    }
+
+  return (
+  <Box>
+   
+    <Input 
+    type="text"
+    name="message"
+    value={username}
+    onChange={ handleChange}
+    />
+    <Button onClick={handleSave}>Add</Button>
+    
+  </Box>
+
+  )
+}
+
+
+export default GameStart
