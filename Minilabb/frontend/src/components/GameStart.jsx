294a98082fc139b4e923e1abb3c3db3fce5b8e3f
alikeici <alialshaikhli323@yahoo.se>
2022-10-17 16:20:24 +0200

    Co-authored-by: Semir Pecar <freemayne@users.noreply.github.com> Co-authored-by: L1nkzel <L1nkzel@users.noreply.github.com>

--- Minilabb/frontend/src/components/GameStart.jsx
+++ Minilabb/frontend/src/components/GameStart.jsx
@@ -1,47 +1,40 @@
-import { useState } from "react"
-import {Box, Button, Input, TextField} from "@mui/material"
-
+import { useState } from "react";
+import { Box, Button, Input } from "@mui/material";
+import React from "react";
 
 const GameStart = () => {
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState("") 
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+    console.log(event.target.value);
+  };
 
-    const handleChange = event => {
-      setUsername(event.target.value);
-      console.log(event.target.value);
-    }
-  
-    
-    const handleSave = async (event) => {
-        const data = {
-            username: username
-        }
+  const handleSave = async (event) => {
+    const data = {
+      username: username,
+    };
 
-        await fetch("http://localhost:5000/user",
-        {
-            method: "POST",
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-        })
-    }
+    await fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
 
   return (
-  <Box>
-   
-    <Input 
-    type="text"
-    name="message"
-    value={username}
-    onChange={ handleChange}
-    />
-    <Button onClick={handleSave}>Add</Button>
-    
-  </Box>
+    <Box>
+      <Input
+        type="text"
+        name="message"
+        value={username}
+        onChange={handleChange}
+      />
+      <Button onClick={handleSave}>Add</Button>
+    </Box>
+  );
+};
 
-  )
-}
-
-
-export default GameStart
+export default GameStart;
